aa435c103365458506620f77ac019062bf651a48
basanthjenuhb <basanthjenuhb@gmail.com>
2017-02-13 14:02:09 +0530

    Complete mating of 2 parents.

--- xor/dna.py
+++ xor/dna.py
@@ -1,5 +1,5 @@
 import random, numpy as np, math
-random.seed(1)
+# random.seed(2)
 class nodeGene:
 	def __init__(self, type_, id_, value):
 		self.type = type_
@@ -14,7 +14,7 @@ class connectGene:
 	def __init__(self, source, destination, weight, enabled, innovation):
 		self.source = source
 		self.destination = destination
-		self.weight = 2 * weight - 1
+		self.weight = weight
 		self.enabled = enabled
 		self.innovation = innovation
 
@@ -23,24 +23,25 @@ class connectGene:
 
 class genes:
 	def __init__(self, inputs, outputs):
-		self.inputs = [ nodeGene("input", i, 1) for i in range(0, inputs) ]
-		self.outputs = [ nodeGene("output",j, 0) for j in range(i+1, outputs + i + 1) ]
+		self.inputs = [ nodeGene("input", i, 0) for i in range(0, inputs) ]
+		self.outputs = [ nodeGene("output",j, 0) for j in range(inputs , outputs + inputs) ]
 		self.nodeGenes = self.inputs + self.outputs
 		self.innovation = 0
 		self.connectGenes = []
 		for source in self.inputs:
 			for destination in self.outputs:
-				self.connectGenes.append(connectGene(source.id, destination.id, random.random(), True, self.innovation))
+				self.connectGenes.append(connectGene(source.id, destination.id, 2 * random.random() - 1, True, self.innovation))
 				self.innovation += 1
 		# for x in self.connectGenes:print x
 
 class neuralNetwork:
 	def __init__(self, inputs, outputs):
-		self.inputs, self.outputs = inputs, outputs
+		self.inputs, self.outputs, self.total = inputs, outputs, inputs + outputs
 		self.gnome = genes(self.inputs, self.outputs)
-		self.score = 0
+		self.score, self.output = 0, []
 
-	def sigmoid(self , z):
+	@staticmethod
+	def sigmoid(z):
 		return 1.0 / ( 1.0 + math.exp(-z) )
 
 	def calculate_backward(self, node):
@@ -48,24 +49,93 @@ class neuralNetwork:
 		inputs = [ [ connect.source , connect.weight ] for connect in self.gnome.connectGenes if connect.destination == node.id and connect.enabled ]
 		val = 0
 		for inp in inputs:val += self.calculate_backward(self.gnome.nodeGenes[inp[0]]) * inp[1]
-		return self.sigmoid(val)
+		node.calculated, node.value = True, self.sigmoid(val)
+		return node.value
 
 	def forward(self, X, Y):
 		if len(X[0]) != len(self.gnome.inputs):
-			print "Input error"
+			print("Input error")
 			return
-		error = []
+		error, self.score, self.output = [], 0, []
 		for i in range(len(X)):
+			for node in self.gnome.nodeGenes:node.value, node.calculated = 0, False
 			for j in range(len(X[i])):
 				self.gnome.inputs[j].value = X[i][j] 
 			for node in self.gnome.outputs:node.value = self.calculate_backward(node)
-			error.append([ node.value for node in self.gnome.outputs ])
-			for node in self.gnome.nodeGenes:node.value = 0
-		self.score = (4 - sum(np.array(error) - Y)) ** 2
-		print error,self.score
+			self.output.append([ node.value for node in self.gnome.outputs ])
+		self.score = (4 - sum(Y - np.array(self.output))) ** 2
+		# print(self.output,self.score)
+
+	def findConnectGene(self, i):
+		connectGene = [ gene for gene in self.gnome.connectGenes if gene.innovation == i ]
+		if len(connectGene):return connectGene[0]
+
+	def randomChoice(self, gene1, gene2):
+		if random.random() < 0.5:
+			return gene1
+		return gene2
+
+	def addDisjointGenes(self, network):
+		disjointGenes = [ gene for gene in network.gnome.connectGenes if gene.innovation >= self.gnome.innovation ]
+		self.gnome.connectGenes += disjointGenes
+
+	def mate(self, network):
+		child = neuralNetwork(2,1)
+		if child.total < max(self.total, network.total):
+			child.gnome.nodeGenes += [ nodeGene("hidden", i, 0) for i in range(child.total, child.total + ( child.total - max(self.total, network.total) )) ]
+		child.gnome.connectGenes, child.gnome.innovation = [], min(self.gnome.innovation, network.gnome.innovation)
+		for i in range(child.gnome.innovation):
+			connectGene1, connectGene2 = self.findConnectGene(i), network.findConnectGene(i)
+			child.gnome.connectGenes.append(self.randomChoice(connectGene1, connectGene2))
+		dominant = self
+		if self.score < network.score:dominant = network
+		child.addDisjointGenes(dominant)
+		return child
+
+	def mutate(self, p1, p2):
+		for connectGene in self.gnome.connectGenes:
+			if random.random() < p1:
+				pass
+
+
+
+class population:
+	def __init__(self, size, X, Y):
+		self.size = size
+		self.X = X
+		self.Y = Y
+		self.networks = [ neuralNetwork(2,1) for i in range(size) ]
+		self.matingPool = []
+		self.best = 0
+
+	def evaluateFitness(self):
+		for network in self.networks:
+			network.forward(self.X, self.Y)
+			if network.score > self.best:self.best = network.score
+
+	def createMatingPool(self):
+		self.matingPool = []
+		for network in self.networks:
+			for i in range(round(int(network.score))):
+				self.matingPool.append(network)
+
+	def newPopulation(self):
+		newpop, poolSize = [], len(self.matingPool)
+		for i in range(self.size):
+			x, y = random.randrange(poolSize), random.randrange(poolSize)
+			newpop.append(self.matingPool[x].mate(self.matingPool[y]))
+		self.networks = newpop[:]
+
+	def mutate(self, p1, p2):
+		for network in self.networks:network.mutate(p1, p2)
 
 X = [ [0,0], [0,1], [1,0], [1,1] ]
 Y = np.array([ [0] , [1] , [1] , [0]])
-size = 10
-networks = [ neuralNetwork(2,1) for i in range(size) ]
-for network in networks:network.forward(X, Y)
+size, p1, p2 = 10, 0.1, 0.5
+p = population(size, X, Y)
+for i in range(10):
+	p.evaluateFitness()
+	p.createMatingPool()
+	p.newPopulation()
+	p.mutate(p1, p2)
+	print(p.best)
